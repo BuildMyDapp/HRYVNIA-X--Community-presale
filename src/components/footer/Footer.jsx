c5d5bf7886efd5d21cae3ea269fcb5c99d6db912
reactDev777 <91951862+reactDev777@users.noreply.github.com>
2021-12-01 08:57:24 +0500

    Changes Done
    Changes Done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,81 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import "./footer.css";
-import twitter from '../../images/twitter.png';
+import twitter from "../../images/twitter.png";
+import facebook from "../../images/facebook.png";
+import discord from "../../images/discord.png";
+import reddit from "../../images/twitter.png";
+
 const Footer = () => {
-    return (
-        <div className="footer mt-5">
-              <div className="container">
-                  <div className="row ">
-                      <div className="col-lg-12 mt-5">
-                         
-                          <h3 className="text-center text-white mt-4">Connect With Us</h3>
-                           <div className="text-center d-flex justify-content-center align-items-center">
-                              <img  className="me-2" src={twitter} width="30" height="30" alt="" />
-                              <h5 className="text-white">Etherscan</h5>
-                              <img className="ms-2" src={twitter} width="30" height="30" alt="" />
+  return (
+    <div className="footer mt-5">
+      <div className="container">
+        <div className="row ">
+         
+          <div className="col-lg-12 mt-5">
+            <h3 className="text-center text-white mt-4">Connect With Us</h3>
+            <div className="text-center d-flex justify-content-center align-items-center">
+              <a href="https://www.facebook.com/">
+                <img
+                  className="me-2"
+                  src={twitter}
+                  width="30"
+                  height="30"
+                  alt=""
+                />
+              </a>
+              <a href="https://www.facebook.com/">
+                <img
+                  className="me-2"
+                  src={facebook}
+                  width="30"
+                  height="30"
+                  alt=""
+                />
+              </a>
 
-                           </div>
-                      </div>
-                      <hr  style={{color:"white",marginTop:"2rem",height:'0.1rem',opacity:'0.5'}}/>
-                  </div>
-                  <div className="row">
-                      <div className="col-lg-12">
-                          <p className="text-center text-white">© 2021 Copyright by hryvnia-x.org. All rights reserved.</p>
-                      </div>
-                  </div>
-              </div>
+              <h5 className="text-white me-2">Etherscan</h5>
+              <a href="https://www.facebook.com/">
+                <img
+                  className="ms-2"
+                  className="discord"
+                  src={discord}
+                  width="30"
+                  height="30"
+                  alt=""
+                />
+              </a>
+
+              <a href="https://www.facebook.com/">
+                <img
+                  className="ms-2"
+                  src={reddit}
+                  width="30"
+                  height="30"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+          <hr
+            style={{
+              color: "white",
+              marginTop: "2rem",
+              height: "0.1rem",
+              opacity: "0.5",
+            }}
+          />
         </div>
-    )
-}
+        <div className="row">
+          <div className="col-lg-12">
+            <p className="text-center text-white">
+              © {new Date().getFullYear()} Copyright by hryvnia-x.org. All
+              rights reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
